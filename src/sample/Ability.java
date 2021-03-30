46a1ec58aefc37f3fce0a31492743e09a6222498
ROMA030 <57528382+ROMA030@users.noreply.github.com>
2021-03-30 11:40:37 +0200

    Update Ability.java

--- src/sample/Ability.java
+++ src/sample/Ability.java
@@ -2,6 +2,7 @@ package sample;
 
 import java.util.ArrayList;
 
+// Questo file ci permetterà di poter costruire degli slot per definire le abilità
 public class Ability {
     public ArrayList<Integer> abilitySlots = new ArrayList<>();
     public int pokemonId;
