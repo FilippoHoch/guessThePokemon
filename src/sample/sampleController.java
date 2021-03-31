457550564177a8c7e733321b282ef37fdfab36a5
ROMA030 <57528382+ROMA030@users.noreply.github.com>
2021-03-31 11:12:56 +0200

    Update sampleController.java

--- src/sample/sampleController.java
+++ src/sample/sampleController.java
@@ -62,7 +62,9 @@ public class sampleController {
     @FXML
     private ListView<String> listView = new ListView<>();
 
-    // Questo metodo raffigura ciò che si vede appena il programma viene fatto partire
+    /* 
+    * Questo metodo raffigura ciò che si vede appena il programma viene fatto partire
+    */
     @FXML
     public void initialize() {
         Image unknownImage = new Image(new File("src/sample/img/unknown.png").toURI().toString());
@@ -74,7 +76,9 @@ public class sampleController {
         conversion.excelReading();
     }
 
-    // Il metodo seguente inizia la parte del codice del programma
+    /* 
+    * Il metodo seguente inizia la parte del codice del programma
+    */
     @FXML
     private void newGame() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample/createNewGame.fxml"));
@@ -94,7 +98,9 @@ public class sampleController {
         });
     }
 
-    // La parte del punteggio del programma viene richiamato tramite il seguente metodo
+    /* 
+    * La parte del punteggio del programma viene richiamato tramite il seguente metodo
+    */
     @FXML
     private void leaderBoards() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample/ranking.fxml"));
@@ -109,7 +115,9 @@ public class sampleController {
         stage.show();
     }
 
-    // Questo metodo nel caso chi stia giocando prova troppe volte, il gioco finisce
+    /* 
+    * Questo metodo nel caso chi stia giocando prova troppe volte, il gioco finisce
+    */
     @FXML
     private void selectElementOfList() {
         String pokemonChoices = listView.getSelectionModel().selectedItemProperty().getValue();
@@ -190,7 +198,9 @@ public class sampleController {
         }
     }
 
-    // La schermata che si visualizza la prima volta che si apre il gioco, è definita nel seguente metodo
+    /* 
+    * La schermata che si visualizza la prima volta che si apre il gioco, è definita nel seguente metodo
+    */
     public void startGame() {
         endGame = false;
         listView.getItems().clear();
